cb900459463c6ab63cc8f4640ececf4f4d139cd1
kungua <52815012+kungua@users.noreply.github.com>
2019-09-05 17:18:27 +0800

    引入 flyway 管理数据库版本
    * 引入 flyway 管理数据库版本

---
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -0,0 +1,17 @@
+create table LINKS_TO_BE_PROCESSED
+(
+    link varchar(1000)
+);
+create table LINKS_TO_ALREADY_PROCESSED
+(
+    link varchar(1000)
+);
+create table NEWS
+(
+    id          bigint primary key auto_increment,
+    title       text,
+    content     text,
+    url         varchar(100),
+    created_at  timestamp,
+    modified_at timestamp
+);
